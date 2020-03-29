69e3a572c6b39fce2a72e5497659b344548807c3
Iwasson <Iwasson@users.noreply.github.com>
2020-03-28 17:03:01 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js')           //js library with commands for discord
+const auth = require('./auth.json')             //auth tokens for logging into bot
+const client = new Discord.Client()             //create a new discord client for the bot
+//const mp3 = require('./INDEED!.mp3')
+
+client.on('ready', () => {
+    //CHANGE THIS TO WHAT CHANNEL YOU WANT TO HAVE THE BOT LISTEN TO
+    console.log("Connected as " + client.user.tag);
+    client.user.setActivity("Listening");
+})
+
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) { return }
+    if (receivedMessage.content.toLowerCase().includes('indeed')) { executeIndeed(receivedMessage) };
+    //sleep(1200);
+    //receivedMessage.member.voice.channel.leave();
+})
+
+async function executeIndeed(message) {
+    console.log("indeed detected");
+
+    if (!message.guild) return;
+
+    if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+
+        const dispatcher = connection.play('indeed.mp3', {
+            volume: 1,
+        });
+    } else {
+        console.log('You need to join a voice channel first!');
+        return;
+
+    }
+}
+
+client.login(auth.token);
